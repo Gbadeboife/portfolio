02cf973b83bf8424836c717250c36e6fbaeabc80
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2024-08-22 18:12:02 +0100

    m

---
+++ api/submit.js
@@ -0,0 +1,25 @@
+function sendMail(event){
+
+    event.preventDefault
+
+    var params={
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+
+    }
+    var msg= document.getElementById('msg')
+    var contactForm= document.getElementById('contact-form')
+
+    var serviceId= 'service_g1aomdf'
+    var templateId= 'template_l5fythe'
+
+    emailjs.send( serviceId , templateId , params ).then(function (res){
+        msg.style.display='block'
+        
+        setTimeout(() => {
+            msg.style.display='none'
+            contactForm.reset()
+        }, 3000)
+    })
+}
